e7bb34020e8645d3c32e870c7d2d3762fe895670
Jayadeep KM <kmjayadeep@gmail.com>
2015-08-29 23:12:33 +0530

    android bugfix

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -6,19 +6,16 @@
 angular.module('Mess', ['ionic', 'Mess.controllers', 'Mess.factories'])
 
 .run(function($ionicPlatform) {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-    }
-
     $ionicPlatform.ready(function() {
-        if (navigator && navigator.splashscreen)
-            navigator.splashscreen.hide();
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
     });
 })
 
